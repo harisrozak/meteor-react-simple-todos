d0ddf8eda6fecfd20fbce88f04a2e81fde8f5cd5
Haris Ainur Rozak <harisrozak@gmail.com>
2022-06-30 22:07:48 +0700

    6: Filter Tags

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from "../api/TasksCollection";
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 
+const toggleChecked = ({ _id, isChecked }) => {
+	TasksCollection.update(_id, {
+		$set: {
+			isChecked: !isChecked
+		}
+
+		/**
+		 * Confused? :) 
+		 * The isChecked prop is the state before click, so it must be the opposite of the clicked value.
+		 */
+	})
+}
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
 export const App = () => {
-	const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+	const [hideCompleted, setHideCompleted] = useState(false);
+	const hideCompletedFilter = { isChecked: { $ne: true } };
+	const tasks = useTracker(
+		() => TasksCollection.find(
+			hideCompleted ? hideCompletedFilter : {},
+			{ sort: { createdAt: -1 } }
+		).fetch()
+	);
+	const pendingTasksCount = useTracker(() =>
+		TasksCollection.find(hideCompletedFilter).count()
+	);
 
-	return(
-		<div>
-			<h1>Welcome to Meteor!</h1>
+	return (
+		<div className="app">
+			<header>
+				<div className="app-bar">
+					<div className="app-header">
+						<h1>
+							📝️ To Do List
+							{pendingTasksCount ? ' (' + pendingTasksCount + ')' : ''}
+						</h1>
+					</div>
+				</div>
+			</header>
 
-			<TaskForm />
+			<div className="main">
+				<TaskForm />
 
-			<ul>
-				{ tasks.map(task => <Task key={ task._id } task={ task } />) }
-			</ul>
+				<div className="filter">
+					<button onClick={() => setHideCompleted(!hideCompleted)}>
+						{hideCompleted ? 'Show All' : 'Hide Completed'}
+					</button>
+				</div>
+
+				<ul className="tasks">
+					{
+						tasks.map(
+							task => <Task
+								key={task._id}
+								task={task}
+								onCheckboxClick={toggleChecked}
+								onDeleteClick={deleteTask}
+							/>
+						)
+					}
+				</ul>
+			</div>
 		</div>
 	);
 };
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const Task = ({ task }) => {
-	return <li>{task.text}</li>
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+	return (
+		<li>
+			<input
+				type="checkbox"
+				id={task._id}
+				checked={!!task.isChecked}
+				onClick={() => onCheckboxClick(task)}
+				readOnly
+			/>
+			<label htmlFor={task._id} >{task.text}</label>
+			<button onClick={() => onDeleteClick(task)}>&times;</button>
+		</li>
+	)
 }
